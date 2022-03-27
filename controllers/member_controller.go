81cad8ca87d83f782a0aa37b2a4b1e4ca6c0b731
Silvi Prisillia <silviprisillia4@yahoo.com>
2022-03-28 01:34:03 +0700

    Add member functions

--- controllers/member_controller.go
+++ controllers/member_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 	"strconv"
 
@@ -13,10 +14,12 @@ func GetAUser(w http.ResponseWriter, r *http.Request) {
 	db := connect()
 	defer db.Close()
 
+	vars := mux.Vars(r)
+	memberId := vars["member_id"]
+
 	query := "SELECT userId, fullName, userName, birthDate, phoneNumber, email, address, password, balance FROM users JOIN members ON users.userId = members.memberId WHERE users.userId=?"
 
-	// nanti get cookies memberId?
-	rows := db.QueryRow(query, 6)
+	rows := db.QueryRow(query, memberId)
 
 	var user model.User
 	if err := rows.Scan(&user.ID, &user.FullName, &user.UserName, &user.BirthDate, &user.PhoneNumber, &user.Email, &user.Address, &user.Password, &user.Balance); err != nil {
@@ -33,7 +36,8 @@ func GetMemberCart(w http.ResponseWriter, r *http.Request) {
 	db := connect()
 	defer db.Close()
 
-	memberId := strconv.Itoa(6)
+	vars := mux.Vars(r)
+	memberId := vars["member_id"]
 
 	query := "SELECT a.bookId, a.coverPath, a.bookTitle, a.author, a.genre, a.year, a.page, a.rentPrice, b.stock, c.branchName FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN carts d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE e.memberId = " + memberId
 
@@ -64,18 +68,91 @@ func GetMemberCart(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateBorrowingList(w http.ResponseWriter, r *http.Request) {
-	// insert new to borrows
-	// delete cart yang udah ga kepake
-	// return
+	db := connect()
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	memberId := vars["member_id"]
+
+	booksId := r.URL.Query()["bookId"]
+
+	err := r.ParseForm()
+	if err != nil {
+		sendNotFoundResponse(w, "Value Not Found")
+		return
+	}
+	borrowDate := r.Form.Get("borrowDate")
+
+	var book model.Book
+	var books []model.Book
+	var stocks = make([]int, len(booksId))
+	var borrowPrice = make([]int, len(booksId))
+	var borrowId int
+
+	for i, bookId := range booksId {
+		query := "SELECT a.bookId, a.coverPath, a.bookTitle, a.author, a.genre, a.year, a.page, a.rentPrice, b.stock, c.branchName FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN carts d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE a.bookId = ? AND e.memberId = ?"
+		rows, err := db.Query(query, bookId, memberId)
+		if err != nil {
+			sendNotFoundResponse(w, "Table Not Found")
+			return
+		}
+		for rows.Next() {
+			if err := rows.Scan(&book.ID, &book.CoverPath, &book.Title, &book.Author, &book.Genre, &book.Year, &book.Page, &book.RentPrice, &book.Stock, &book.BranchName); err != nil {
+				sendBadRequestResponse(w, "Error Field Undefined")
+				return
+			} else {
+				books = append(books, book)
+			}
+		}
+		query2 := "SELECT b.stockId, a.rentPrice FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN carts d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE a.bookId = ? AND e.memberId = ?"
+		rows2 := db.QueryRow(query2, bookId, memberId)
+		if err := rows2.Scan(&stocks[i], &borrowPrice[i]); err != nil {
+			log.Println(err)
+			sendBadRequestResponse(w, "Error Field Undefined")
+			return
+		}
+
+		if i == 0 {
+			result, errQuery := db.Exec("INSERT INTO borrows(memberId, borrowDate, returnDate, borrowPrice, borrowState) VALUES (?, ?, ?, ?, ?)", memberId, borrowDate, "0000-00-00 00:00:00", 0, "PROCESSED")
+			if errQuery != nil {
+				log.Println(errQuery)
+				return
+			}
+			lastBorrowId, _ := result.LastInsertId()
+			borrowId = int(lastBorrowId)
+		}
+
+		_, errQuery := db.Exec("UPDATE borrows SET borrowPrice = borrowPrice + ? WHERE borrowId = ?", borrowPrice[i], borrowId)
+		if errQuery != nil {
+			log.Println(errQuery)
+			return
+		} else {
+			_, errQuery := db.Exec("INSERT INTO borrowslist VALUES (?, ?)", borrowId, stocks[i])
+			if errQuery != nil {
+				log.Println(errQuery)
+				return
+			} else {
+				_, errQuery := db.Exec("DELETE FROM carts WHERE memberId = ? AND stockId = ?", memberId, stocks[i])
+				if errQuery != nil {
+					log.Println(errQuery)
+					return
+				}
+			}
+		}
+	}
+	sendSuccessResponse(w, "Insert Success", books)
+
+	db.Close()
 }
 
 func GetOngoingBorrowing(w http.ResponseWriter, r *http.Request) {
 	db := connect()
 	defer db.Close()
 
-	memberId := strconv.Itoa(6)
+	vars := mux.Vars(r)
+	memberId := vars["member_id"]
 
-	query := "SELECT a.bookId, a.coverPath, a.bookTitle, a.author, a.genre, a.year, a.page, a.rentPrice, b.stock, c.branchName FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN borrows d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE WHERE d.borrowState = 'ON-GOING' AND  e.memberId = " + memberId
+	query := "SELECT a.bookId, a.coverPath, a.bookTitle, a.author, a.genre, a.year, a.page, a.rentPrice, b.stock, c.branchName FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN borrows d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE d.borrowState = 'ON-GOING' AND  e.memberId = " + memberId
 
 	rows, err := db.Query(query)
 	if err != nil {
@@ -114,7 +191,6 @@ func EditUserProfile(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// dari cookies
-	// userID := 6
 	fullName := "Ariesta Leevine"
 	userName := "ariestacsleevine"
 	birthDate := "2020-04-09"
@@ -123,7 +199,7 @@ func EditUserProfile(w http.ResponseWriter, r *http.Request) {
 	address := "Jalan Peta No. 241"
 
 	vars := mux.Vars(r)
-	userID := vars["member_id"]
+	userId := vars["member_id"]
 
 	if fullName != r.Form.Get("fullName") {
 		fullName = r.Form.Get("fullName")
@@ -140,7 +216,7 @@ func EditUserProfile(w http.ResponseWriter, r *http.Request) {
 	}
 
 	result, errQuery := db.Exec("UPDATE users SET fullName=?, userName=?, birthDate=?, phone=?, email=?, address=?", fullName, userName, birthDate, phone, email, address)
-	rows, _ := db.Query("SELECT userId, fullName, userName, birthDate, phoneNumber, email, address, password, balance FROM users JOIN members ON users.userId = members.memberId WHERE users.userId=?", userID)
+	rows, _ := db.Query("SELECT userId, fullName, userName, birthDate, phoneNumber, email, address, password, balance FROM users JOIN members ON users.userId = members.memberId WHERE users.userId=?", userId)
 
 	num, _ := result.RowsAffected()
 
@@ -177,9 +253,6 @@ func EditUserPassword(w http.ResponseWriter, r *http.Request) {
 		sendNotFoundResponse(w, "Value Not Found")
 		return
 	}
-
-	// dari cookies
-	// userID := 6
 
 	vars := mux.Vars(r)
 	userID := vars["member_id"]
@@ -226,7 +299,7 @@ func TopupUserBalance(w http.ResponseWriter, r *http.Request) {
 
 	// dari cookies
 	// userID := 6
-	//balance get dari cookies
+	// balance get dari cookies
 
 	vars := mux.Vars(r)
 	userID := vars["member_id"]
@@ -303,7 +376,8 @@ func GetMemberHistory(w http.ResponseWriter, r *http.Request) {
 	db := connect()
 	defer db.Close()
 
-	memberId := strconv.Itoa(6)
+	vars := mux.Vars(r)
+	memberId := vars["member_id"]
 
 	query := "SELECT a.bookId, a.coverPath, a.bookTitle, a.author, a.genre, a.year, a.page, a.rentPrice, b.stock, c.branchName FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN borrows d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE d.borrowState = 'ON-GOING' OR d.BorrowState = 'FINISHED' AND  e.memberId = " + memberId
 

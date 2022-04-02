6eebdb59f056031696d03a1889e05d2516a3e542
Silvi Prisillia <silviprisillia4@yahoo.com>
2022-04-02 15:43:28 +0700

    Update member functions

--- controllers/member_controller.go
+++ controllers/member_controller.go
@@ -4,18 +4,16 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"Perpustakaan-HB/model"
-
-	"github.com/gorilla/mux"
 )
 
 func GetAUser(w http.ResponseWriter, r *http.Request) {
 	db := connect()
 	defer db.Close()
 
-	vars := mux.Vars(r)
-	memberId := vars["member_id"]
+	memberId := getIdFromCookies(r)
 
 	query := "SELECT userId, fullName, userName, birthDate, phoneNumber, email, address, password, balance FROM users JOIN members ON users.userId = members.memberId WHERE users.userId=?"
 
@@ -36,12 +34,11 @@ func GetMemberCart(w http.ResponseWriter, r *http.Request) {
 	db := connect()
 	defer db.Close()
 
-	vars := mux.Vars(r)
-	memberId := vars["member_id"]
+	memberId := getIdFromCookies(r)
 
-	query := "SELECT a.bookId, a.coverPath, a.bookTitle, a.author, a.genre, a.year, a.page, a.rentPrice, b.stock, c.branchName FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN carts d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE e.memberId = " + memberId
+	query := "SELECT a.bookId, a.coverPath, a.bookTitle, a.author, a.genre, a.year, a.page, a.rentPrice, b.stock, c.branchName FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN carts d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE e.memberId = ?"
 
-	rows, err := db.Query(query)
+	rows, err := db.Query(query, memberId)
 	if err != nil {
 		sendNotFoundResponse(w, "Table Not Found")
 		return
@@ -71,8 +68,7 @@ func CreateBorrowingList(w http.ResponseWriter, r *http.Request) {
 	db := connect()
 	defer db.Close()
 
-	vars := mux.Vars(r)
-	memberId := vars["member_id"]
+	memberId := getIdFromCookies(r)
 
 	booksId := r.URL.Query()["bookId"]
 
@@ -149,12 +145,11 @@ func GetOngoingBorrowing(w http.ResponseWriter, r *http.Request) {
 	db := connect()
 	defer db.Close()
 
-	vars := mux.Vars(r)
-	memberId := vars["member_id"]
+	memberId := getIdFromCookies(r)
 
-	query := "SELECT a.bookId, a.coverPath, a.bookTitle, a.author, a.genre, a.year, a.page, a.rentPrice, b.stock, c.branchName FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN borrows d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE d.borrowState = 'ON-GOING' AND  e.memberId = " + memberId
+	query := "SELECT a.bookId, a.coverPath, a.bookTitle, a.author, a.genre, a.year, a.page, a.rentPrice, b.stock, c.branchName FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN borrows d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE d.borrowState = 'ON-GOING' AND  e.memberId = ?"
 
-	rows, err := db.Query(query)
+	rows, err := db.Query(query, memberId)
 	if err != nil {
 		sendNotFoundResponse(w, "Table Not Found")
 		return
@@ -190,32 +185,25 @@ func EditUserProfile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// dari cookies
-	fullName := "Ariesta Leevine"
-	userName := "ariestacsleevine"
-	birthDate := "2020-04-09"
-	phone := "0898-1234-1234"
-	email := "ariestacsleevine"
-	address := "Jalan Peta No. 241"
-
-	vars := mux.Vars(r)
-	userId := vars["member_id"]
+	userId, fullName, userName, birthDate, phone, email, address, additionalAddress, _, _ := getDataFromCookies(r)
 
 	if fullName != r.Form.Get("fullName") {
 		fullName = r.Form.Get("fullName")
 	} else if userName != r.Form.Get("userName") {
 		userName = r.Form.Get("userName")
-	} else if birthDate != r.Form.Get("birthDate") {
-		birthDate = r.Form.Get("birthDate")
+	} else if time.Time.String(birthDate) != r.Form.Get("birthDate") {
+		birthDate, _ = time.Parse("2020-01-01", r.Form.Get("birthDate"))
 	} else if phone != r.Form.Get("phone") {
 		phone = r.Form.Get("phone")
 	} else if email != r.Form.Get("email") {
 		email = r.Form.Get("email")
 	} else if address != r.Form.Get("address") {
 		address = r.Form.Get("address")
+	} else if additionalAddress != r.Form.Get("additionalAddress") {
+		additionalAddress = r.Form.Get("additionalAddress")
 	}
 
-	result, errQuery := db.Exec("UPDATE users SET fullName=?, userName=?, birthDate=?, phone=?, email=?, address=?", fullName, userName, birthDate, phone, email, address)
+	result, errQuery := db.Exec("UPDATE users SET fullName=?, userName=?, birthDate=?, phone=?, email=?, address=?, additionalAddress=?", fullName, userName, birthDate, phone, email, address, additionalAddress)
 	rows, _ := db.Query("SELECT userId, fullName, userName, birthDate, phoneNumber, email, address, password, balance FROM users JOIN members ON users.userId = members.memberId WHERE users.userId=?", userId)
 
 	num, _ := result.RowsAffected()
@@ -254,12 +242,11 @@ func EditUserPassword(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	vars := mux.Vars(r)
-	userID := vars["member_id"]
-	password := r.Form.Get("password")
+	userId, _, _, _, _, _, _, _, password, _ := getDataFromCookies(r)
+	password = encodePassword(password)
 
-	result, errQuery := db.Exec("UPDATE users SET password=? WHERE memberId=?", password, userID)
-	rows, _ := db.Query("SELECT userId, fullName, userName, birthDate, phoneNumber, email, address, password, balance FROM users JOIN members ON users.userId = members.memberId WHERE users.userId=?", userID)
+	result, errQuery := db.Exec("UPDATE users SET password=? WHERE memberId=?", password, userId)
+	rows, _ := db.Query("SELECT userId, fullName, userName, birthDate, phoneNumber, email, address, password, balance FROM users JOIN members ON users.userId = members.memberId WHERE users.userId=?", userId)
 
 	num, _ := result.RowsAffected()
 
@@ -297,17 +284,13 @@ func TopupUserBalance(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// dari cookies
-	// userID := 6
-	// balance get dari cookies
+	userId, _, _, _, _, _, _, _, _, balance := getDataFromCookies(r)
 
-	vars := mux.Vars(r)
-	userID := vars["member_id"]
 	newBalance, _ := strconv.Atoi(r.Form.Get("balance"))
-	balance := 50000 + newBalance
+	balance = balance + newBalance
 
-	result, errQuery := db.Exec("UPDATE users SET balance=? WHERE memberId=?", balance, userID)
-	rows, _ := db.Query("SELECT userId, fullName, userName, birthDate, phoneNumber, email, address, password, balance FROM users JOIN members ON users.userId = members.memberId WHERE users.userId=?", userID)
+	result, errQuery := db.Exec("UPDATE users SET balance=? WHERE memberId=?", balance, userId)
+	rows, _ := db.Query("SELECT userId, fullName, userName, birthDate, phoneNumber, email, address, password, balance FROM users JOIN members ON users.userId = members.memberId WHERE users.userId=?", userId)
 
 	num, _ := result.RowsAffected()
 
@@ -339,10 +322,9 @@ func DeleteAccount(w http.ResponseWriter, r *http.Request) {
 	db := connect()
 	defer db.Close()
 
-	vars := mux.Vars(r)
-	userID := vars["userID"]
+	userId := getIdFromCookies(r)
 
-	rows, _ := db.Query("SELECT userId, fullName, userName, birthDate, phoneNumber, email, address, password, balance FROM users JOIN members ON users.userId = members.memberId WHERE users.userId=?", userID)
+	rows, _ := db.Query("SELECT userId, fullName, userName, birthDate, phoneNumber, email, address, password, balance FROM users JOIN members ON users.userId = members.memberId WHERE users.userId=?", userId)
 
 	var member model.Member
 	var members []model.Member
@@ -355,14 +337,15 @@ func DeleteAccount(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	_, errQuery := db.Exec("DELETE FROM members WHERE id=?", userID)
+	_, errQuery := db.Exec("DELETE FROM members WHERE id=?", userId)
 	if errQuery == nil {
-		result, errQuery := db.Exec("DELETE FROM users WHERE id=?", userID)
+		result, errQuery := db.Exec("DELETE FROM users WHERE id=?", userId)
 		num, _ := result.RowsAffected()
 		if errQuery == nil {
 			if num == 0 {
 				sendBadRequestResponse(w, "Error 0 Rows Affected")
 			} else {
+				resetUserToken(w)
 				sendSuccessResponse(w, "Delete Success", members)
 			}
 		} else {
@@ -379,12 +362,11 @@ func GetMemberHistory(w http.ResponseWriter, r *http.Request) {
 	db := connect()
 	defer db.Close()
 
-	vars := mux.Vars(r)
-	memberId := vars["member_id"]
+	memberId := getIdFromCookies(r)
 
-	query := "SELECT a.bookId, a.coverPath, a.bookTitle, a.author, a.genre, a.year, a.page, a.rentPrice, b.stock, c.branchName FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN borrows d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE d.borrowState = 'ON-GOING' OR d.BorrowState = 'FINISHED' AND  e.memberId = " + memberId
+	query := "SELECT a.bookId, a.coverPath, a.bookTitle, a.author, a.genre, a.year, a.page, a.rentPrice, b.stock, c.branchName FROM books a JOIN stocks b ON a.bookId = b.bookId JOIN branches c ON b.branchId = c.branchId JOIN borrows d ON b.stockId = d.stockId JOIN members e ON d.memberId = e.memberId WHERE d.borrowState = 'ON-GOING' OR d.BorrowState = 'FINISHED' AND  e.memberId = "
 
-	rows, err := db.Query(query)
+	rows, err := db.Query(query, memberId)
 	if err != nil {
 		sendNotFoundResponse(w, "Table Not Found")
 		return

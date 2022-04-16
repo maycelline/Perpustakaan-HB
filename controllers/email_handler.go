8604313c87d9e1324649b5ed8f16a7aec249518b
Feliciana Gunadi <isabel.feli27gunadi@gmail.com>
2022-04-16 22:12:11 +0700

    overdue check first

--- controllers/email_handler.go
+++ controllers/email_handler.go
@@ -117,6 +117,70 @@ func WeeklyEmailScheduler() {
 	var users []model.User = GetAllUsers()
 
 	for i := 0; i < len(users); i++ {
-		SetScheduler(users[i].Email)
+		SetEmailWeeklyScheduler(users[i].Email)
+	}
+}
+
+func CheckUserBorrowing() ([]model.UserBorrowing, bool) {
+	db := connect()
+	defer db.Close()
+
+	var users []model.User = GetAllUsers()
+	var userBorrow model.UserBorrowing
+	var usersBorrow []model.UserBorrowing
+	for i := 0; i < len(users); i++ {
+		query := "SELECT borrowId FROM borrows WHERE borrows.memberId = ? HAVING (TIMESTAMPDIFF(WEEK, borrows.borrowDate, CURDATE()) >= 2)"
+
+		var book model.Book
+		var books []model.Book
+		var borrow model.Borrowing
+		var borrows []model.Borrowing
+
+		rows, err := db.Query(query, users[i].ID)
+
+		if err != nil {
+			return nil, false
+		}
+
+		for rows.Next() {
+			if err := rows.Scan(); err != nil {
+				return nil, false
+			} else {
+				var stockId int
+				query := "SELECT * FROM borrowslist WHERE borrowslist.borrowId = ? AND borrowslist.borrowState = 'BORROWED' OR borrowslist.borrowState = 'OVERDUE'"
+				rows2, err := db.Query(query, borrow.ID)
+
+				if err != nil {
+					return nil, false
+				}
+
+				for rows2.Next() {
+					if err := rows2.Scan(&borrow.ID, &stockId); err != nil {
+						return nil, false
+					} else {
+						query := "SELECT books.* FROM books JOIN stocks WHERE stocks.stockId = ?"
+						rows3 := db.QueryRow(query, stockId)
+						rows3.Scan()
+						_, err := db.Exec("UPDATE borrowslist SET borrowState = 'OVERDUE' WHERE borrowId = ? AND bookId = ?", borrow.ID, book.ID)
+						if err != nil {
+							return nil, false
+						}
+
+						books = append(books, book)
+					}
+				}
+				borrow.Book = books
+				borrows = append(borrows, borrow)
+			}
+		}
+		userBorrow.UserData = users[i]
+		userBorrow.UserBorrowingData = borrows
+		usersBorrow = append(usersBorrow, userBorrow)
+	}
+
+	if len(usersBorrow) < 1 {
+		return nil, false
+	} else {
+		return usersBorrow, true
 	}
 }
--- controllers/scheduler.go
+++ controllers/scheduler.go
@@ -1,11 +1,25 @@
 package controllers
 
-import "github.com/jasonlvhit/gocron"
+import (
+	"github.com/jasonlvhit/gocron"
+)
 
-func SetScheduler(email string) {
+func SetEmailWeeklyScheduler(email string) {
 	scheduler := gocron.NewScheduler()
 	scheduler.Every(1).Week().Do(func() {
 		SendWeeklyEmail(email)
 	})
 	scheduler.Start()
+}
+
+func SetEmailBorrowingInfoScheduler() {
+	var schedulerOverdue = gocron.NewScheduler()
+	schedulerOverdue.Every(1).Day().Do(func() {
+		userBorrowsData, status := CheckUserBorrowing()
+		if status {
+			for i := 0; i < len(userBorrowsData); i++ {
+				SendWeeklyEmail(userBorrowsData[i].UserData.Email)
+			}
+		}
+	})
 }
